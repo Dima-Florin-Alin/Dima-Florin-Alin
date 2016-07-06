2b4e7444ee1dbe97dd6d8ebe3038f77f9d55cf93
Ionut Lepadatescu <ionut.lepadatescu@qualitance.com>
2016-07-06 15:55:37 +0300

    Validate register form

---
+++ app/register/register.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded',function(){
+    var registerForm = document.querySelector('#register');
+    var inputs = ['#username','#email','#password','#confirm-password'];
+    for(var i=0;i<inputs.length;i++) {
+      document.querySelector(inputs[i]).addEventListener("keyup",function() {
+        onchangeElem('#' + this.id)
+      })
+    }
+    registerForm.addEventListener('submit', function(event){
+        var hasError = false;
+        for(var i=0;i<inputs.length;i++) {
+          if(isEmptyInput(inputs[i])) {
+             hasError = true;
+             showError(inputs[i])
+          } else {
+            hideError(inputs[i])
+          }
+        }
+
+        if(hasError) {
+          event.preventDefault();
+        }
+    })
+})
+
+function onchangeElem(inputId) {
+  isEmptyInput(inputId) ? showError(inputId) : hideError(inputId);
+}
+
+function showError(inputId) {
+  var parentNode =  document.querySelector(inputId).parentNode;
+  parentNode.className +=  ' has-error';
+  parentNode.querySelector('.help-block').className = parentNode.querySelector('.help-block').className.replace('hidden','');
+}
+function hideError(inputId) {
+  var parentNode =  document.querySelector(inputId).parentNode;
+  parentNode.className = parentNode.className.replace('has-error','');
+  parentNode.querySelector('.help-block').className +=' hidden';
+}
+function isEmptyInput(inputId) {
+  return !document.querySelector(inputId).value
+}
